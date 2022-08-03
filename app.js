751bb7a433b3c691b8217966364af4bfc115d329
jeshu2002 <71264878+jeshu2002@users.noreply.github.com>
2022-08-03 19:52:36 +0530

    Updating files

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express  = require("express")
 const https = require("https")
 const bodyParser = require("body-parser")
-
+  
 const app = express()
-
+app.use(express.static("public"))
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.get("/", function(req, res) {
